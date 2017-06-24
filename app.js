6b84b6cd034b93d6f2addcf26bc7743f41f2c4c7
Jason Clifton <j.clifton1473@gmail.com>
2017-06-24 13:02:39 -0600

    added basic create user page

---
+++ app.js
@@ -0,0 +1,31 @@
+const postURL = 'http://localhost:3000/api/v1/users';
+$(appReady)
+
+function appReady() {
+  getUserInput();
+}
+function getUserInput() {
+  $('.create-account').submit(function(event) {
+    event.preventDefault();
+    const userInfo = getUserValues();
+    console.log(userInfo);
+    $.post(postURL, userInfo)
+      .then(function(result) {
+      console.log(result);
+    })
+    .catch(function(result){
+      console.log(result);
+    });
+  })
+}
+function getUserValues() {
+  let userName = $('.user-name').val();
+  let userEmail = $('.user-email').val();
+  let userPassword = $('.user-password').val();
+  const userInfo = {
+    "name": userName,
+    "email": userEmail,
+    "password": userPassword
+  }
+  return userInfo;
+}
